a2b7a44114ecdfdbc1bb9c8f23872ee4e0cc06f7
Ford Pickert <fordpickert@gmail.com>
2023-05-23 16:50:44 -0500

    simple deploy script, hh config set, deploy to testnet next

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config({ path: '.env' });
+
+const QUICKNODE_URL = process.env.QUICKNODE;
+const PRIVATE_KEY = process.env.PVT_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+	solidity: '0.8.18',
+	networks: {
+		goerli: {
+			url: QUICKNODE_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
 };
